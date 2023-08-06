c1a2d038751f3606dbb25e9c413cb83419c05b0a
amrashour <a.ashour@msd-my.com>
2023-08-06 16:18:46 +0300

    added hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { Hero } from '@/components'
+
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      amr
+      <Hero />
     </main>
   )
 }
